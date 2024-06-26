994af1969342e15048697c17485749c36f9468d6
minwang03 <93901911+minwang03@users.noreply.github.com>
2024-06-26 11:09:48 +0700

    Tạo văn phòng thành công

---
+++ src/main/java/com/vexeexpress/vexeexpressserver/repository/BmsOfficeRepository.java
@@ -0,0 +1,13 @@
+package com.vexeexpress.vexeexpressserver.repository;
+
+import com.vexeexpress.vexeexpressserver.entity.BmsOffice;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BmsOfficeRepository extends JpaRepository<BmsOffice, Long> {
+     List<BmsOffice> findByCompanyId(Long companyId);
+}
