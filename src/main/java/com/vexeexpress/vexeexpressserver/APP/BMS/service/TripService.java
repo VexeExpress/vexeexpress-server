2c7caace77bac868c7be5214433d027aef0cf4a5
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2024-07-10 15:34:22 +0700

    update short trip

--- src/main/java/com/vexeexpress/vexeexpressserver/APP/BMS/service/TripService.java
+++ src/main/java/com/vexeexpress/vexeexpressserver/APP/BMS/service/TripService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TripService {
@@ -27,9 +28,19 @@ public class TripService {
     public List<BmsTrip> searchTrips(String valueRouter, String valueDayDeparture, String companyId) {
         System.out.println("Received request: valueRouter=" + valueRouter + ", valueDayDeparture=" + valueDayDeparture + ", companyId=" + companyId);
         List<BmsTrip> listTrip = tripRepository.findByCompanyId(companyId);
-        System.out.println("Found trips: " + listTrip);
+//        System.out.println("Found trips: " + listTrip);
+        System.out.println("Value Day Departure: " + valueDayDeparture);
 
-        return null;
+        List<BmsTrip> listValueRouterTrip = tripRepository.findByValueRouter(valueRouter);
+        System.out.println("Found value router: " + listValueRouterTrip);
+        LocalDate date = LocalDate.parse(valueDayDeparture, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        System.out.println("Date short: " + date);
+
+        List<BmsTrip> filteredTrips = listValueRouterTrip.stream()
+                .filter(trip -> trip.getValueDayDeparture().isEqual(date))
+                .toList();
+        System.out.println("filteredTrips: " + filteredTrips);
+        return filteredTrips;
     }
 
     public List<BmsTrip> getAllTrips() {
--- src/main/java/com/vexeexpress/vexeexpressserver/repository/TripRepository.java
+++ src/main/java/com/vexeexpress/vexeexpressserver/repository/TripRepository.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 @Repository
 public interface TripRepository extends JpaRepository<BmsTrip, String> {
-    List<BmsTrip> findByValueRouter(String companyId);
+    List<BmsTrip> findByValueRouter(String valueRouter);
 
     List<BmsTrip> findByCompanyId(String companyId);
 }
